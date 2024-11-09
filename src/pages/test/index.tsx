47e0e4dda573d4a80de19512d4f23058c6d6821c
0ai3 <david.a.danciu@gmail.com>
2024-11-09 04:27:27 +0200

    refactor Forex component to fetch and display forex prices, update total balance calculation, and improve currency pair selection

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,13 +1,12 @@
-// App.tsx or any parent component
+
 import React from 'react';
-import Crypto from '@/components/Crypto';
 import Forex from '@/components/Forex';
 
 const App: React.FC = () => {
 
   return (
     <div>
-        <Crypto />
+        <Forex />
     </div>
   );
 };
