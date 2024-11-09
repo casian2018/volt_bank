dbdd4078a74b3fa56f3bfd6e696c3b960bc98e3e
CasianTheProgrammator <99408730+casian2018@users.noreply.github.com>
2024-11-09 18:11:36 +0200

    update

--- src/components/Forex.tsx
+++ src/components/Forex.tsx
@@ -4,10 +4,45 @@ import Chart from "./Chart"; // Assuming you have this component
 import PieChart from "./PieChart";
 
 const Forex: React.FC = () => {
-  const [forexBalances, setForexBalances] = useState<{ [key: string]: number }>({});
+  const [forexBalances, setForexBalances] = useState<{ [key: string]: number }>(
+    {}
+  );
   const [usdBalances, setUsdBalances] = useState<{ [key: string]: number }>({});
   const [selectedChartPair, setSelectedChartPair] = useState("EURUSD");
   const [total, setTotal] = useState(0);
+
+  const addNewSymbol = async () => {
+    const newSymbol = prompt("Enter a new currency  (e.g., EUR):");
+    const initialBalance = parseFloat("0");
+
+    if (newSymbol && !isNaN(initialBalance)) {
+      try {
+        // Update the local state for instant feedback
+        setForexBalances((prevBalances) => ({
+          ...prevBalances,
+          [newSymbol.toUpperCase()]: initialBalance,
+        }));
+
+        // Save the new balance in the database
+        const response = await fetch("/api/forex-balances", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            symbol: newSymbol.toUpperCase().slice(0, 3),
+            balance: 0,
+          }),
+        });
+
+        if (!response.ok) {
+          throw new Error("Failed to add new currency pair");
+        }
+
+        setSelectedChartPair(newSymbol.toUpperCase()+'USD');
+      } catch (error) {
+        console.error("Error adding new symbol:", error);
+      }
+    }
+  };
 
   useEffect(() => {
     const fetchForexBalances = async () => {
@@ -58,51 +93,76 @@ const Forex: React.FC = () => {
       <div className="flex gap-12">
         <div className="mb-8 bg-white p-6 shadow-lg rounded-xl mt-6 w-full">
           <h2 className="text-xl font-semibold text-gray-800">Total Balance</h2>
-          <p className="text-3xl font-bold text-indigo-600 mt-2">${total.toFixed(2)}</p>
+          <p className="text-3xl font-bold text-indigo-600 mt-2">
+            ${total.toFixed(2)}
+          </p>
 
           {Object.entries(forexBalances).map(([symbol, balance]) => (
             <div key={symbol} className="flex justify-between">
               <p className="text-gray-600 font-medium">{symbol}:</p>
               <p className="text-gray-800 font-medium">
-                {balance.toFixed(2)} ({usdBalances[symbol]?.toFixed(2) ?? "0.00"} USD)
+                {balance.toFixed(2)} (
+                {usdBalances[symbol]?.toFixed(2) ?? "0.00"} USD)
               </p>
             </div>
           ))}
         </div>
 
         <div className="mt-6 bg-white p-6 shadow-lg rounded-xl w-fit mb-8">
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">Portfolio Allocation</h2>
+          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+            Portfolio Allocation
+          </h2>
           <PieChart balances={forexBalances} />
         </div>
       </div>
 
       <div className="flex flex-col lg:flex-row gap-6">
         <div className="flex flex-col w-full lg:w-2/3 bg-white p-6 shadow-lg rounded-xl">
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">Market Chart</h2>
+          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+            Market Chart
+          </h2>
           <Chart symbol={selectedChartPair} />
         </div>
 
         <div className="flex flex-col w-full lg:w-1/3 bg-white p-6 shadow-lg rounded-xl">
-          <h2 className="text-xl font-semibold text-gray-800 mb-4">Select Currency Pair</h2>
-          <div className="space-y-3">
-            {["EURUSD", "GBPUSD", "USDJPY", "AUDUSD"].map((pair) => (
-              <div
-                key={pair}
-                onClick={() => setSelectedChartPair(pair)}
-                className={`flex items-center justify-between bg-gray-100 p-3 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-200 cursor-pointer`}
+          <div className="flex flex-col">
+            <div className="flex justify-between pb-4">
+              <h2 className="text-xl font-semibold text-gray-800 mb-4">
+                Select Currency Pair
+              </h2>
+              <button
+                className="border-2 py-2 px-4 rounded-lg hover:bg-indigo-600 hover:text-white transition"
+                onClick={addNewSymbol}
               >
-                <p className="text-gray-700 font-medium">View {pair} Chart</p>
-                <button
-                  className={`px-4 py-2 rounded-lg font-semibold text-sm ${
-                    selectedChartPair === pair
-                      ? "bg-indigo-600 text-white"
-                      : "bg-indigo-50 text-indigo-600 border border-indigo-500 hover:bg-indigo-100"
-                  } transition duration-150`}
-                >
-                  {pair}
-                </button>
-              </div>
-            ))}
+                Add
+              </button>
+            </div>
+            <div className="space-y-3 flex flex-col">
+              {Object.keys(forexBalances).map((currency) => {
+                if (currency === "USD") return null;
+                const pair = currency + "USD";
+                return (
+                  <div
+                    key={pair}
+                    onClick={() => setSelectedChartPair(pair)}
+                    className={`flex items-center justify-between bg-gray-100 p-3 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-200 cursor-pointer`}
+                  >
+                    <p className="text-gray-700 font-medium">
+                      View {pair} Chart
+                    </p>
+                    <button
+                      className={`px-4 py-2 rounded-lg font-semibold text-sm ${
+                        selectedChartPair === pair
+                          ? "bg-indigo-600 text-white"
+                          : "bg-indigo-50 text-indigo-600 border border-indigo-500 hover:bg-indigo-100"
+                      } transition duration-150`}
+                    >
+                      {pair}
+                    </button>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
--- src/pages/api/forex-balances.js
+++ src/pages/api/forex-balances.js
@@ -3,36 +3,62 @@ import jwt from 'jsonwebtoken';
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-async function getForexBalances(req, res) {
-    if (req.method !== 'GET') {
-        return res.status(405).json({ message: 'Method Not Allowed' });
+async function forexBalances(req, res) {
+  // Get the token from cookies
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  try {
+    // Verify the token
+    const decoded = jwt.verify(token, JWT_SECRET);
+    const email = decoded.email;
+
+    const client = await clientPromise;
+    const db = client.db('volt_bank');
+    const usersCollection = db.collection('users');
+    const user = await usersCollection.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
     }
 
-    // Get the token from cookies
-    const token = req.cookies.token;
+    if (req.method === 'GET') {
+      // Retrieve forexBalances
+      if (user.forexBalances) {
+        return res.status(200).json(user.forexBalances);
+      } else {
+        return res.status(404).json({ message: 'No balances available' });
+      }
+    } else if (req.method === 'POST') {
+      // Add a new currency pair with an initial balance
+      const { symbol, balance } = req.body;
 
-    if (!token) {
-        return res.status(401).json({ message: 'No token provided' });
+      if (!symbol || typeof balance !== 'number') {
+        return res.status(400).json({ message: 'Invalid symbol or balance' });
+      }
+
+      // Update user's forexBalances
+      const updatedForexBalances = {
+        ...user.forexBalances,
+        [symbol.toUpperCase()]: balance,
+      };
+
+      await usersCollection.updateOne(
+        { email },
+        { $set: { forexBalances: updatedForexBalances } }
+      );
+
+      return res.status(200).json({ message: 'Currency pair added', forexBalances: updatedForexBalances });
+    } else {
+      return res.status(405).json({ message: 'Method Not Allowed' });
     }
-
-    try {
-        // Verify the token
-        const decoded = jwt.verify(token, JWT_SECRET);
-        const email = decoded.email;
-
-        const client = await clientPromise;
-        const db = client.db('volt_bank');
-        const user = await db.collection('users').findOne({ email });
-
-        if (user && user.forexBalances) {
-            return res.status(200).json(user.forexBalances);
-        } else {
-            return res.status(404).json({ message: 'User not found or no balances available' });
-        }
-    } catch (error) {
-        console.error('Error verifying token or fetching balances:', error);
-        return res.status(500).json({ message: 'Error verifying token or fetching balances' });
-    }
+  } catch (error) {
+    console.error('Error processing request:', error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
 }
 
-export default getForexBalances;
+export default forexBalances;
